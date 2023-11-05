76a568586d281bb8180d9e4a0ad1b5476f85132e
olssonbianca <olssonbiancanadine@gmail.com>
2023-11-05 01:22:46 -0300

    components

---
+++ src/components/User.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+        <img src='' alt=''/>
+        <h6>
+
+        </h6>
+        <p>
+
+        </p>
+    </div>
+  )
+}
+
+export default User
